9a4b1044c02a71bdb9b379e00f442644ac1b184a
ryuta.sato <rodman8290@gmail.com>
2020-07-23 05:48:46 +0000

    task-list login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
-    before_action :require_user_logged_in, only: [:index, :show]
+    before_action :require_user_logged_in, only: [:index, :show ]
+    before_action :correct_user, only: [:destroy]
     def index
-        @tasks = Task.all
+        if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end
 
   def show
@@ -13,10 +17,11 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+     
+      @task = current_user.tasks.build(task_params)
       if @task.save
           flash[:success] = "Taskが正常に投稿されました"
-          redirect_to @task
+          redirect_to root_url
       else
           flash.now[:danger] = "Taskが投稿されませんでした"
           render :new
@@ -40,16 +45,21 @@ class TasksController < ApplicationController
   end
 
   def destroy
-      @task = Task.find(params[:id])
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private 
   
   def task_params
       params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+        redirect_to root_url
+    end
   end
 end
