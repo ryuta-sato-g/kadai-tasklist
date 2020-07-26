84cad0fb4b6ad1858ca4410f3601b0217ac43c54
ryuta.sato <rodman8290@gmail.com>
2020-07-26 10:59:00 +0000

    correct_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in, only: [:index, :show,]
-    before_action :correct_user, only: [:destroy,]
+    before_action :correct_user, only: [:destroy, :edit]
     def index
         if logged_in?
             @task = current_user.tasks.build
